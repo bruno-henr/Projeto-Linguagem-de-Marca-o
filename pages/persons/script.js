59bd7f66fb6e5f348de0e65368ffa0c5076a380f
bruno-henr <brunohenriquex00@gmail.com>
2023-02-15 00:14:39 -0300

    feat: criando animação

--- pages/persons/script.js
+++ pages/persons/script.js
@@ -1,3 +1,26 @@
-document.body.onload = () => {
-    // var div
-}
+// window.addEventListener('scroll', () => {
+//     // console.log('aaaa', document.querySelectorAll('.person-item'));
+
+//     document.querySelectorAll('.person-item').forEach((item, index) => {
+//         console.log('=>',item.getBoundingClientRect().top, window.innerHeight)
+//         if(item.getBoundingClientRect().top+ 100 < window.innerHeight ) {
+//             // console.log(item.classList);
+//             item.classList.add("active-item");
+//             // document.getElementById("sidebar").classList.add("active-sidebar");
+//         }
+//     })
+// })
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddens = document.querySelectorAll('.hidden');
+// console.log(hiddens)
+hiddens.forEach((el) => observer.observe(el))
--- script.js
+++ script.js
@@ -6,3 +6,16 @@ document.getElementById("toggle-menu").addEventListener("click", function() {
         document.getElementById("sidebar").classList.add("active-sidebar");
     }
 })
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddens = document.querySelectorAll('.hidden');
+hiddens.forEach((el) => observer.observe(el))
